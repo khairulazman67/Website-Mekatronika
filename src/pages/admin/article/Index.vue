81f48d635bdcfd04abba55244b9425719bf80351
khairulazman67 <azman.khairul67@gmail.com>
2022-12-19 14:17:02 +0700

    add search

--- src/pages/admin/article/Index.vue
+++ src/pages/admin/article/Index.vue
@@ -36,8 +36,17 @@
                                                 </button>
                                             </router-link>
 
-                                            <div class="mt-5">
-                                                <div class="overflow-x-auto ">
+                                            <div class="mt-5 ">
+                                                <div class="flex justify-start">
+                                                    <div class="relative w-1/4">
+                                                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                                                        </div>
+                                                        <input @change="searchData" v-model="search" type="search" id="default-search" class="block w-full p-4 pl-10 text-sm  text-gray-700 border border-gray-800 rounded-lg bg-gray-50 focus:ring-primary-700 focus:border-primary-800" placeholder="Search" required>
+                                                        <!-- <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> -->
+                                                    </div>
+                                                </div>
+                                                <div class="overflow-x-auto mt-4">
                                                     <table
                                                         class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                                                         <thead
@@ -102,6 +111,33 @@
                                                         </tbody>
                                                     </table>
                                                 </div>
+                                            <!-- pagination -->
+                                                <nav class="mt-5 flex justify-center">
+                                                    <ul class="inline-flex -space-x-px">
+                                                        <li>
+                                                            <a href="#" class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">Previous</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">1</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#" aria-current="page" class="px-3 py-2 text-primary-800 border border-gray-300 bg-red-50 hover:bg-red-100 hover:text-primary-700">3</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">4</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">5</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">Next</a>
+                                                        </li>
+                                                    </ul>
+                                                </nav>
+
                                             </div>
                                         </div>
                                     </div>
@@ -121,33 +157,42 @@
     import Swal from 'sweetalert2'
     var moment = require('moment');
     import axios from 'axios';
+
     export default {
         components: {
             Sidebar
         },
+
+
+
         data() {
             return {
                 showMenu: true,
                 data: 0,
                 moment: moment,
+                search:''
             }
         },
+        
         methods: {
             toggleNav() {
                 this.showMenu = this.showMenu === false ? true : false
             },
-            async getData() {
+            async getData(data) {
+                console.log('masok')
                 await axios
-                    .get(`contents`)
+                    .get(`contents/`, {params:{search:data}})
                     .then(r => {
                         this.data = r.data.data
-                        console.log(this.data);
+                        console.log('data ini bro',this.data);
                     }).catch(e => {
                         console.dir(e);
                     });
             },
-            getContent() {
-                console.log('ini konten', this.content);
+            async searchData() {
+                console.log('ini konten', this.search);
+                await this.getData(this.search)
+                console.log('masok')
             },
             deleteContent(id){
                 console.log('id', id)
