413ec87080339dedec431335db2bea8e1cf4254c
khairulazman67 <azman.khairul67@gmail.com>
2022-12-19 23:15:58 +0700

    add pagination

--- src/pages/admin/article/Index.vue
+++ src/pages/admin/article/Index.vue
@@ -42,7 +42,7 @@
                                                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                                             <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                                                         </div>
-                                                        <input @change="searchData" v-model="search" type="search" id="default-search" class="block w-full p-4 pl-10 text-sm  text-gray-700 border border-gray-800 rounded-lg bg-gray-50 focus:ring-primary-700 focus:border-primary-800" placeholder="Search" required>
+                                                        <input v-model="search" type="search" id="default-search" class="block w-full p-4 pl-10 text-sm  text-gray-700 border border-gray-800 rounded-lg bg-gray-50 focus:ring-primary-700 focus:border-primary-800" placeholder="Search" required>
                                                         <!-- <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> -->
                                                     </div>
                                                 </div>
@@ -115,25 +115,19 @@
                                                 <nav class="mt-5 flex justify-center">
                                                     <ul class="inline-flex -space-x-px">
                                                         <li>
-                                                            <a href="#" class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">Previous</a>
+                                                            <a href="#" @click="setPagination(0)" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"><i class="fa-solid fa-angles-left"></i></a>
                                                         </li>
                                                         <li>
-                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">1</a>
+                                                            <a href="#" @click="setPagination('-')" class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"><i class="fa-solid fa-angle-left"></i> </a>
+                                                        </li>
+                                                        <li  v-for="(v,i) in pagination.page" :key="i">
+                                                            <a href="#" @click="setPagination(i)"  class="px-3 py-2  " :class="[pagination.pageNow ==i? 'text-primary-800 border border-gray-300 bg-red-50 hover:bg-red-100 hover:text-primary-700' : 'leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700']">{{i+1}}</a>
                                                         </li>
                                                         <li>
-                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</a>
+                                                            <a href="#" @click="setPagination('+')" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"><i class="fa-solid fa-angle-right"></i></a>
                                                         </li>
                                                         <li>
-                                                            <a href="#" aria-current="page" class="px-3 py-2 text-primary-800 border border-gray-300 bg-red-50 hover:bg-red-100 hover:text-primary-700">3</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">4</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">5</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">Next</a>
+                                                            <a href="#" @click="setPagination(pagination.page-1)" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"><i class="fa-solid fa-angles-right"></i></a>
                                                         </li>
                                                     </ul>
                                                 </nav>
@@ -162,15 +156,30 @@
         components: {
             Sidebar
         },
-
-
-
         data() {
             return {
+                isActive:true,
                 showMenu: true,
                 data: 0,
                 moment: moment,
-                search:''
+                search:'',
+
+                value1: '',
+                result: 0,
+
+                pagination :{
+                    total : null,
+                    page :1,
+                    pageNow : 0,
+                    limit : 10,
+                    offset : 0
+                }
+            }
+        },
+
+        watch: {
+            search: function () {
+                this.getData(this.search)
             }
         },
         
@@ -178,24 +187,33 @@
             toggleNav() {
                 this.showMenu = this.showMenu === false ? true : false
             },
-            async getData(data) {
-                console.log('masok')
+
+            async setPagination(page){
+                if(page=='+'){
+                    if(this.pagination.pageNow<this.pagination.page-1)
+                        this.pagination.pageNow = this.pagination.pageNow+1
+                }else if(page=='-' ){
+                    if(this.pagination.pageNow-1>=0)
+                        this.pagination.pageNow = this.pagination.pageNow-1
+                }else{
+                    this.pagination.pageNow = await page
+                }
+                this.pagination.offset=await this.pagination.pageNow*this.pagination.limit
+                this.getData('',await this.pagination.offset)
+            },
+
+            async getData(search) {
                 await axios
-                    .get(`contents/`, {params:{search:data}})
+                    .get(`contents/`, {params:{search,offset:this.pagination.offset,limit:this.pagination.limit}})
                     .then(r => {
                         this.data = r.data.data
-                        console.log('data ini bro',this.data);
+                        this.pagination.total = r.data.total
+                        this.pagination.page = Math.ceil(this.pagination.total/this.pagination.limit)
                     }).catch(e => {
                         console.dir(e);
                     });
             },
-            async searchData() {
-                console.log('ini konten', this.search);
-                await this.getData(this.search)
-                console.log('masok')
-            },
             deleteContent(id){
-                console.log('id', id)
                 Swal.fire({
                     title: `Apakah anda yakin untuk menghapus data ini?`,
                     showCancelButton: true,
@@ -205,9 +223,7 @@
                         axios
                             .delete(`contents/${id}`)
                             .then(r => {
-                                console.log(r)
                                 if (r.data.status === 'success') {
-                                    console.log(r)
                                     Swal.fire(
                                         'Berhasil!',
                                         'Data ketegori berhasil disimpan!',
